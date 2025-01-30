cc074c0b18dd8bf1f360d39ac894f64d58e28e9a
Luka Lah <luka1.lah@gmail.com>
2025-01-30 20:10:18 +0100

    included DTO but not needed

---
+++ Users.Application/Users/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Users.Application.Users.Dtos;
+// Currently not used, but if we would want to display only the things form User that we want this is the way to go ...
+// Since this is a class, we could map User type over UserDto and display desired info
+// Automapper is fastest way for mapping
+internal class UserDto
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = default!;
+    public string FullName { get; set; } = default!;
+    public string Language { get; set; } = default!;
+    public string Culture { get; set; } = default!;
+}
