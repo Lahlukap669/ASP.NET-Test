28e4cdbeefcd98717ba97c0b3c5b183907aa6fc6
Luka Lah <luka1.lah@gmail.com>
2025-01-30 21:53:03 +0100

    CreateUsers validation From entity

--- Users.Application/Users/Dtos/CreateUserDto.cs
+++ Users.Application/Users/Dtos/CreateUserDto.cs
@@ -1,14 +1,23 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace Users.Application.Users.Dtos;
 
 public class CreateUserDto
 {
+    [StringLength(100, MinimumLength = 4)]
     public string UserName { get; set; } = default!;
     public string FullName { get; set; } = default!;
+
+    [EmailAddress(ErrorMessage = "Please provide a valid email address!")]
     public string Email { get; set; } = default!;
+    
+    //[Phone(ErrorMessage = "Please provide a valid phone address!")]
     public string Mobile { get; set; } = default!;
     public string Language { get; set; } = default!;
     public string Culture { get; set; } = default!;
     public string Password { get; set; } = default!;
 }
+
+//DataAnnotations allow regex
+//Better costum validation will be with FluentValidation (NuGet) -> expecialy for category values
